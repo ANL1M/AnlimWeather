7bbe862e349cf176699d07a6b1cccf346503fab2
Андрей Лименько <anlimenko@live.ru>
2022-05-20 19:18:52 +0200

    - Deleted old icons - Added Splash start screen

---
+++ app/src/main/java/anlim/weather/StartActivity.kt
@@ -0,0 +1,12 @@
+package anlim.weather
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@StartActivity, MainActivity::class.java))
+    }
+}
